5595b989e69b607d7b20341326dee27329934b26
Alex Pan <alexanderdavidpan@gmail.com>
2017-08-04 17:54:19 -0700

    Create set_exchange helper method and move it into its own module.
    - Extendable across other crytocompare modules that use exchange as a parameter.
    - Remove duplicate code and extend helper method where needed.

---
+++ lib/cryptocompare/helpers/exchange_name_helper.rb
@@ -0,0 +1,12 @@
+require 'yaml'
+
+# Helper module for exchange names.
+module ExchangeNameHelper
+  EXCHANGES = YAML::load_file(File.join(__dir__, '../../../config/exchanges.yml'))
+
+  # Helper method to overcome case-sensitive exchange name enforced by the API.
+  # If no supported exchange mapping is found, it will try user's input.
+  def self.set_exchange(exchange)
+    EXCHANGES[exchange.upcase] || exchange
+  end
+end

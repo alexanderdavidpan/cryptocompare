6d7b4a7b14ce930991dce7cc0d7d40afae10d4fb
Alex Pan <alexanderdavidpan@gmail.com>
2017-08-12 15:31:41 -0700

    Add unit tests and fixtures for HistoMinute optional params.
    - Add test and fixtures for limit param to limit results.
    - Add test and fixtures for agg param for aggregated/unaggregated results.
    - Add test and fixtures for to_ts param for final data point time.

--- test/test_histo_minute.rb
+++ test/test_histo_minute.rb
@@ -29,4 +29,72 @@ class TestHistoMinute < Minitest::Test
       end
     end
   end
+
+  def test_find_histo_minute_cryptocurrency_to_fiat_limit_option
+    # Limit 1 - Will return 2 data points
+    VCR.use_cassette('btc_to_usd_histo_minute_with_limit_1') do
+      histo_minute_resp = Cryptocompare::HistoMinute.find('BTC', 'USD', {'limit' => 1})
+
+      assert histo_minute_resp.kind_of?(Hash)
+      assert_equal 'Success', histo_minute_resp['Response']
+
+      histo_minute_data = histo_minute_resp['Data']
+      assert histo_minute_data
+      assert histo_minute_data.kind_of?(Array)
+      assert_equal 2, histo_minute_data.length
+    end
+
+    # Limit 10 - Will return 11 data points
+    VCR.use_cassette('btc_to_usd_histo_minute_with_limit_10') do
+      histo_minute_resp = Cryptocompare::HistoMinute.find('BTC', 'USD', {'limit' => 10})
+
+      assert histo_minute_resp.kind_of?(Hash)
+      assert_equal 'Success', histo_minute_resp['Response']
+
+      histo_minute_data = histo_minute_resp['Data']
+      assert histo_minute_data
+      assert histo_minute_data.kind_of?(Array)
+      assert_equal 11, histo_minute_data.length
+    end
+  end
+
+  def test_find_histo_minute_cryptocurrency_to_fiat_agg_option
+    # Unaggregated
+    VCR.use_cassette('btc_to_usd_histo_minute_unaggregated') do
+      unaggregated_histo_minute_resp = Cryptocompare::HistoMinute.find('BTC', 'USD', {'agg' => 1})
+
+      assert unaggregated_histo_minute_resp.kind_of?(Hash)
+      assert_equal 'Success', unaggregated_histo_minute_resp['Response']
+
+      refute unaggregated_histo_minute_resp['Aggregated']
+    end
+
+    # Aggregated
+    VCR.use_cassette('btc_to_usd_histo_minute_aggregated') do
+      unaggregated_histo_minute_resp = Cryptocompare::HistoMinute.find('BTC', 'USD', {'agg' => 2})
+
+      assert unaggregated_histo_minute_resp.kind_of?(Hash)
+      assert_equal 'Success', unaggregated_histo_minute_resp['Response']
+
+      assert unaggregated_histo_minute_resp['Aggregated']
+    end
+  end
+
+  def test_find_histo_minute_cryptocurrency_to_fiat_to_ts_option
+    VCR.use_cassette('btc_to_usd_histo_minute_with_to_ts') do
+      to_timestamp = 1502515020
+
+      histo_minute_resp = Cryptocompare::HistoMinute.find('BTC', 'USD', {'to_ts' => to_timestamp})
+
+      assert histo_minute_resp.kind_of?(Hash)
+      assert_equal 'Success', histo_minute_resp['Response']
+      assert_equal to_timestamp, histo_minute_resp['TimeTo']
+
+      histo_minute_data = histo_minute_resp['Data']
+      assert histo_minute_data
+      assert histo_minute_data.kind_of?(Array)
+
+      assert_equal to_timestamp, histo_minute_data.last['time']
+    end
+  end
 end

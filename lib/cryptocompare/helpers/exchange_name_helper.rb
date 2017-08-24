a67f312fa8fd9844665ce98d03095fcc91dfb7d0
Alex Pan <alexanderdavidpan@gmail.com>
2017-08-23 21:08:39 -0700

    Move ExchangeNameHelper to Cryptocompare namespace.

--- lib/cryptocompare/helpers/exchange_name_helper.rb
+++ lib/cryptocompare/helpers/exchange_name_helper.rb
@@ -1,12 +1,14 @@
 require 'yaml'
 
 # Helper module for exchange names.
-module ExchangeNameHelper
-  EXCHANGES = YAML::load_file(File.join(__dir__, '../../../config/exchanges.yml'))
+module Cryptocompare
+  module ExchangeNameHelper
+    EXCHANGES = YAML::load_file(File.join(__dir__, '../../../config/exchanges.yml'))
 
-  # Helper method to overcome case-sensitive exchange name enforced by the API.
-  # If no supported exchange mapping is found, it will try user's input.
-  def self.set_exchange(exchange)
-    EXCHANGES[exchange.upcase] || exchange
+    # Helper method to overcome case-sensitive exchange name enforced by the API.
+    # If no supported exchange mapping is found, it will try user's input.
+    def self.set_exchange(exchange)
+      EXCHANGES[exchange.upcase] || exchange
+    end
   end
 end
--- test/test_exchange_name_helper.rb
+++ test/test_exchange_name_helper.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class TestExchangeNameHelper < Minitest::Test
   def test_set_exchange_name_case_insensitive
-    ExchangeNameHelper::EXCHANGES.each do |_, v|
+    Cryptocompare::ExchangeNameHelper::EXCHANGES.each do |_, v|
       random_casing = v.gsub(/./){|s| s.send(%i[upcase downcase].sample)}
-      assert_equal v, ::ExchangeNameHelper.set_exchange(random_casing)
+      assert_equal v, Cryptocompare::ExchangeNameHelper.set_exchange(random_casing)
     end
   end
 
   def test_set_exchange_name_tries_user_input_if_no_exchange_mapping_found
-    assert_equal 'YoloTrade', ::ExchangeNameHelper.set_exchange('YoloTrade')
+    assert_equal 'YoloTrade', Cryptocompare::ExchangeNameHelper.set_exchange('YoloTrade')
   end
 end

93b42d89a2e978f2384660b565ba0aef08158151
Alex Pan <alexanderdavidpan@gmail.com>
2017-08-30 19:12:22 -0700

    Add generate_avg class method to Price module.
    - Add new query param mapping for market param.
    - Refactor code to allow for multiple API urls in Price module.
    - Add documentation for generate_avg method.
    - Minor spacing edits.

--- lib/cryptocompare/price.rb
+++ lib/cryptocompare/price.rb
@@ -3,7 +3,8 @@ require 'json'
 
 module Cryptocompare
   module Price
-    API_URL = 'https://min-api.cryptocompare.com/data/pricemulti'
+    PRICE_API_URL = 'https://min-api.cryptocompare.com/data/pricemulti'
+    GENERATE_AVG_API_URL = 'https://min-api.cryptocompare.com/data/generateAvg'
 
     # Finds the currency price(s) of a given currency symbol. Really fast,
     # 20-60 ms. Cached each 10 seconds.
@@ -61,10 +62,90 @@ module Cryptocompare
     def self.find(from_syms, to_syms, opts = {})
       params = {
         'from_syms' => Array(from_syms).join(','),
-        'to_syms' => Array(to_syms).join(',')
+        'to_syms'   => Array(to_syms).join(',')
       }.merge!(opts)
 
-      full_path = QueryParamHelper.set_query_params(API_URL, params)
+      full_path = QueryParamHelper.set_query_params(PRICE_API_URL, params)
+      api_resp = Faraday.get(full_path)
+      JSON.parse(api_resp.body)
+    end
+
+    # Compute the current trading info (price, vol, open, high, low, etc) of the
+    # requested pair as a volume weighted average based on the markets that are
+    # requested.
+    #
+    # ==== Parameters
+    #
+    # * +from_sym+  [String]        - (required) currency symbols  (ex: 'BTC', 'ETH', 'LTC', 'USD', 'EUR', 'CNY')
+    # * +to_sym+    [String]        - (required) currency symbols  (ex: 'USD', 'EUR', 'CNY', 'USD', 'EUR', 'CNY')
+    # * +markets+   [String, Array] - (required) Name of exchanges. Supports multiple. (ex: 'Coinbase','Poloniex')
+    # * +opts+      [Hash]          - (optional) options hash
+    #
+    # ==== Options
+    #
+    # * +tc+        [Boolean]       - (optional) try conversion. Default true. If the crypto does not trade directly into the toSymbol requested, BTC will be used for conversion.
+    #
+    # ==== Returns
+    #
+    # [Hash] Hash that returns two hashes: raw and display. Both contain similar
+    #        data, one is just raw data and the other is meant for displaying.
+    #        See sample response below for data that is returned.
+    #
+    # ==== Examples
+    #
+    # Generate average price for cryptocurrency to fiat currency.
+    #
+    #   Cryptocompare::Price.generate_avg('BTC', 'USD', ['Coinbase', 'Bitfinex'])
+    #
+    # Sample response
+    #
+    #   {
+    #     RAW: {
+    #       MARKET: "CUSTOMAGG",
+    #       FROMSYMBOL: "BTC",
+    #       TOSYMBOL: "USD",
+    #       FLAGS: 0,
+    #       PRICE: 4137.43,
+    #       LASTUPDATE: 1503454563,
+    #       LASTVOLUME: 2,
+    #       LASTVOLUMETO: 8271.98,
+    #       LASTTRADEID: 19656029,
+    #       VOLUME24HOUR: 71728.71957884016,
+    #       VOLUME24HOURTO: 279374718.3442189,
+    #       OPEN24HOUR: 3885.85,
+    #       HIGH24HOUR: 4145,
+    #       LOW24HOUR: 3583.46,
+    #       LASTMARKET: "Coinbase",
+    #       CHANGE24HOUR: 251.58000000000038,
+    #       CHANGEPCT24HOUR: 6.474259171095137
+    #     },
+    #     DISPLAY: {
+    #       FROMSYMBOL: "Ƀ",
+    #       TOSYMBOL: "$",
+    #       MARKET: "CUSTOMAGG",
+    #       PRICE: "$ 4,137.43",
+    #       LASTUPDATE: "Just now",
+    #       LASTVOLUME: "Ƀ 2",
+    #       LASTVOLUMETO: "$ 8,271.98",
+    #       LASTTRADEID: 19656029,
+    #       VOLUME24HOUR: "Ƀ 71,728.7",
+    #       VOLUME24HOURTO: "$ 279,374,718.3",
+    #       OPEN24HOUR: "$ 3,885.85",
+    #       HIGH24HOUR: "$ 4,145",
+    #       LOW24HOUR: "$ 3,583.46",
+    #       LASTMARKET: "Coinbase",
+    #       CHANGE24HOUR: "$ 251.58",
+    #       CHANGEPCT24HOUR: "6.47"
+    #     }
+    #   }
+    def self.generate_avg(from_sym, to_sym, markets, opts = {})
+      params = {
+        'from_sym' => from_sym,
+        'to_sym'   => to_sym,
+        'markets'  => Array(markets).join(',')
+      }.merge!(opts)
+
+      full_path = QueryParamHelper.set_query_params(GENERATE_AVG_API_URL, params)
       api_resp = Faraday.get(full_path)
       JSON.parse(api_resp.body)
     end
